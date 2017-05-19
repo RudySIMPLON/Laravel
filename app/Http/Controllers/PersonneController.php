f6a573dd448d6c47df90e67189faa0ca14c5bf9a
simplon <rudy__10@hotmail.fr>
2017-05-19 16:56:34 +0200

    ajout methode get_table

--- app/Http/Controllers/PersonneController.php
+++ app/Http/Controllers/PersonneController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class PersonneController extends Controller
 {
-    //
+	public function get_table (){
+
+		$personnes = Personne::All();
+		return view('list',compact('personnes'));
+	}
 }
