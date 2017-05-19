e8e8008b1d8621ad9d57ef7e9a5913cdbf55f61e
simplon <rudy__10@hotmail.fr>
2017-05-19 21:24:45 +0200

    ajout chemin vue detail

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@
 
 Route::get('/','PersonneController@get_table');
 
+Route::get('detail','PersonneController@return_detail');
+
 Route::Post('/add', 'PersonneController@add_people');
 
 Route::get('/edit/{id}','PersonneController@edit_form');
 
 
 Route::Post('/editCheck/{id}','PersonneController@check_edit');
+
+Route::get('/delete/{id}','PersonneController@delete');
