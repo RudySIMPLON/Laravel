28fb9ac139fd0f3dc4018b721e09eb4c8436586e
simplon <rudy__10@hotmail.fr>
2017-05-19 20:27:35 +0200

    ajout vue editer

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('base')
+
+@section('content')
+<h1>Modification </h1>
+
+<div class="col-sm-4">
+	<form action="/editCheck/{{$personnes->id}}" method="post">
+		{{csrf_field()}}
+		<label for="surname">Nom
+			<input type="text" name="surname" id="surname" value="{{$personnes->surname}}">
+		</label>
+
+		<label for="name">Prenom
+			<input type="text" name="name" id="name" value="{{$personnes->name}}">
+		</label>
+
+		<label for="email">email
+			<input type="email" name="email" id="email" value="{{$personnes->email}}">
+		</label>
+
+		<label for="birthday">Date de naissance
+			<input type="" name="birthday" id="birthday" value="{{$personnes->birthday}}">
+		</label>
+		<input type="submit">
+	</form>
+</div> 
+
+
+
+@stop
