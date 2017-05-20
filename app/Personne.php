36292e4edb86e3124cc0e8cf2a58ea373a15a59c
simplon <rudy__10@hotmail.fr>
2017-05-20 20:30:23 +0200

    ajout protected personne

--- app/Personne.php
+++ app/Personne.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Personne extends Model
 {
-    //
+    protected $fillable = array('name', 'surname', 'birthday', 'email');
+     protected $table = 'personnes';
 }
