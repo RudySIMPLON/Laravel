3c74e62b196d18bfdf4c8f3ad0fcecf358c4295b
simplon <rudy__10@hotmail.fr>
2017-05-21 20:07:51 +0200

    changement button

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -7,53 +7,59 @@
     <h1>Liste Utilisateur</h1><br>
 
     <form action="/detail" method="get">
-        <button value="">Ajouter un Utilisateur</button>
+        <button class="btn btn-success">Ajouter un Utilisateur</button>
     </form>    
 
 </div><br><br>
 
 <table id="myTable" class="table-stripped table table-bordered">
 
+    
     <thead>
-
         <tr>
             <th>Prénom</th>
             <th>Nom</th>
             <th>Date de Naissance</th>
             <th>Email</th>
+            <th></th>
+            <th></th>
 
         </tr>
+   </thead>
 
-    </thead>
     <tbody>
         @foreach ( $personnes as $personne) 
+        
         <tr>
-            <th>{{$personne->name}}</th>
-            <th>{{$personne->surname}}</th>
-            <th>{{$personne->birthday}}</th>
-            <th>{{$personne->email}}</th>
+            <td>{{$personne->name}}</td>
+            <td>{{$personne->surname}}</td>
+            <td>{{$personne->birthday}}</td>
+            <td>{{$personne->email}}</td>
             <th>
-                <form action="/edit/{{$personne->id}}" method="get">
-                    <button class="btn btn-primary">modifier</button>
+              <form action="/edit/{{$personne->id}}" method="get">
+                {{csrf_field()}}
+                    <button class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></button>
                 </form>
-            </th>
+            </td>
             <th><form action="/delete/{{$personne->id}}">
-                <button class="btn btn-danger">Supprimer</button>
+                {{csrf_field()}}
+                <button class="btn btn-danger delete"><span class="glyphicon glyphicon-remove"></span></button>
             </form>
-        </th>
+        </td>
 
     </tr>
+  
 
 
     @endforeach
 </tbody>
 </table>
 
-<script src="//code.jquery.com/jquery.js"></script>
+        <script src="//code.jquery.com/jquery.js"></script>
         
         <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
         
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
 
 <script>
 
